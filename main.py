97ac9e5dcde50d370097cfa21bb98b58f6228e95
Sudharsan Vaidhun <sudharsan.vaidhun@knights.ucf.edu>
2022-03-13 18:57:46 -0400

    More config parameters

--- main.py
+++ main.py
@@ -7,27 +7,30 @@ from taskAnalyser import SchedulabilityTest
 try:
     with open('sim.cfg', 'r') as fh:
         config = json.load(fh)
+        numOfTasks = config['numOfTasks']
         iterations = range(config['numOfIterations'])
         critProbs = config['critProbs']
         wcetRatios = config['wcetRatios']
         minDeadlineRatios = config['minDeadlineRatios']
+        totalUtilizations = config['totalUtilizations']
 except FileNotFoundError:
     config = None
+    numOfTasks = 10
     iterations = range(10)
     critProbs = [0.5]
     wcetRatios = [2,3]
     minDeadlineRatios = [0.7, 0.8]
+    totalUtilizations = [0.1, 0.3, 0.5, 0.7]
 
-for iter, critProb, wcetRatio, minDeadlineRatio in itertools.product(
-    iterations, [0.5],
-    [2,3], [0.7, 0.8]):
+for totalUtilization, iter, critProb, wcetRatio, minDeadlineRatio in itertools.product(
+    totalUtilizations, iterations, critProbs, wcetRatios, minDeadlineRatios):
 
     taskSet = TaskGen().genTask('Uunifast',
-            numOfTasks=10,
-            totalUtilization=0.9,
-            critProb = 0.5,
-            wcetRatio = 2,
-            minDeadlineRatio = 0.7)
+            numOfTasks=numOfTasks,
+            totalUtilization=totalUtilization,
+            critProb = critProb,
+            wcetRatio = wcetRatio,
+            minDeadlineRatio = minDeadlineRatio)
     
     # if config['VERBOSE']:
     #     taskSet.listTasks()
@@ -35,8 +38,8 @@ for iter, critProb, wcetRatio, minDeadlineRatio in itertools.product(
     solver = SchedulabilityTest(taskSet, 60, 60, 60, config)
     solver.solve()
     if solver.scalingFactor < 0:
-        printFormat = '#{:3d} | P = {:4.2f} | R = {:5.2f} | D = {:4.2f} | x = ----- <<< Fail!'
+        printFormat = 'U = {:5.3f} | #{:3d} | P = {:4.2f} | R = {:5.2f} | D = {:4.2f} | x = ----- <<< Fail!'
     else:
-        printFormat = '#{:3d} | P = {:4.2f} | R = {:5.2f} | D = {:4.2f} | x = {:5.3f}'
+        printFormat = 'U = {:5.3f} | #{:3d} | P = {:4.2f} | R = {:5.2f} | D = {:4.2f} | x = {:5.3f}'
     
-    print(printFormat.format(iter, critProb, wcetRatio, minDeadlineRatio, solver.scalingFactor))
+    print(printFormat.format(totalUtilization, iter, critProb, wcetRatio, minDeadlineRatio, solver.scalingFactor))
